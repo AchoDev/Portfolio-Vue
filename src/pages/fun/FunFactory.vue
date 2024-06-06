b0fdb60b49c40b2d9ae12ebbca5ace0fbbe586fb
AchoDev <ahmedhdasi89@gmail.com>
2024-06-06 17:09:24 +0800

    fun factory things now

--- src/pages/fun/FunFactory.vue
+++ src/pages/fun/FunFactory.vue
@@ -4,6 +4,26 @@
 
         <p>This page is where the true fun begins. Lots of utilities and things to play around with <br><br> Nah just kidding, it's literally just debug tools I use</p>
 
-
+        <ParticleSystemMaker />
     </div>
 </template>
+
+<script setup lang="ts">
+
+import ParticleSystemMaker from './ParticleSystemMaker.vue';
+
+</script>
+
+<style scoped lang="scss">
+
+#container {
+    margin-top: 50px;
+    text-align: center;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+</style>
---
+++ src/pages/fun/ParticleSystemMaker.vue
@@ -0,0 +1,62 @@
+<template>
+
+<div id="container">
+    <div id="controls">
+        <div>
+            Speed:
+            <input v-model="speed" type="range" min="0" max="10">
+            {{ speed }}
+        </div>
+        
+        <div>
+            Size:
+            <input v-model="size" type="range" min="0" max="10">
+            {{ size }}
+        </div>
+    </div>
+
+    <ParticleSystem 
+        :speed="speed"
+        :size="size"
+    />
+
+</div>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ParticleSystem from '../../components/ParticleSystem.vue';
+
+const speed = ref<number>(0)
+const size = ref<number>(0)
+
+</script>
+
+<style scoped lang="scss">
+
+#container {
+    background: rgba(255, 255, 255, 0.237);
+    backdrop-filter: blur(100px);
+    max-width: 700px;
+    height: 300px;
+    padding: 10px;
+
+    display: flex;
+    flex-direction: row !important;
+    justify-content: start !important;
+
+    #controls {
+        width: auto;
+        display: flex;
+        flex-direction: column;
+
+        div {
+            width: 230px;
+            display: flex;
+            justify-content: space-between;
+        }
+    }
+}
+
+</style>
